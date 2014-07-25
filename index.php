6b82d03d483941a89c4c4bcecec97ff6a9c210d8
scott2020 <scott@wolffm.com>
2014-07-25 13:48:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,22 @@
 <html>
-Shootout Results 2014
-<br>
-<?php
-include('config.php');
-include('register_globals.php');
-register_globals();
-?>
 
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta name="GENERATOR" content="Notepad">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include '../config.php';?>
 <title>Reports - <?php echo $SITE_NAME;?></title>
+</head>
+
 <body>
-
-<div data-role="page" id="page">
-    <div data-role="header">
-        <h1>Page One</h1>
-    </div>
-    <div data-role="content">    
-        <ul data-role="listview">
-            <li><a href="#page2">Page Two</a></li>
-            <li><a href="#page3">Page Three</a></li>
-            <li><a href="#page4">Page Four</a></li>
-        </ul>        
-    </div>
-    <div data-role="footer">
-        <h4>Page Footer</h4>
-    </div>
-</div>
-
-<div data-role="page" id="page2">
-    <div data-role="header">
-        <h1>Page Two</h1>
-    </div>
-    <div data-role="content">    
-        Content        
-    </div>
-    <div data-role="footer">
-        <h4>Page Footer</h4>
-    </div>
-</div>
-
-<div data-role="page" id="page3">
-    <div data-role="header">
-        <h1>Page Three</h1>
-    </div>
-    <div data-role="content">    
-        Content        
-    </div>
-    <div data-role="footer">
-        <h4>Page Footer</h4>
-    </div>
-</div>
-
-<div data-role="page" id="page4">
-    <div data-role="header">
-        <h1>Page Four</h1>
-    </div>
-    <div data-role="content">    
-        Content        
-    </div>
-    <div data-role="footer">
-        <h4>Page Footer</h4>
-    </div>
-</div>
+<br>
+Shootout 2014 Results
+<br>
+<p>&nbsp;</p>
+<p><font face="Arial">Reports</font></p>
+<p><font face="Arial"><a href="byclass.php">Top Scores grouped by class</a></font></p>
+<p><font face="Arial"><a href="bycompetitor.php">All runs sorted by competitor</a></font></p>
 
 </body>
+
 </html>
