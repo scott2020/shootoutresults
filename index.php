a45ea7eccc4bd357b6fbf7b6bf61e89eeca9d071
scott2020 <scott@wolffm.com>
2014-07-25 13:52:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Language" content="en-us">
 <meta name="GENERATOR" content="Notepad">
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<?php include '../config.php';?>
+<?php include 'config.php';?>
 <title>Reports - <?php echo $SITE_NAME;?></title>
 </head>
 
@@ -13,7 +13,8 @@
 Shootout 2014 Results
 <br>
 <p>&nbsp;</p>
-<p><font face="Arial">Reports</font></p>
+<p><font face="Arial">Welcome to the Lake Shootout 2014 LIVE results page!</font></p>
+<p><font face="Arial">Please choose one of the links below to see up to the minute Shootout results.</font></p>
 <p><font face="Arial"><a href="byclass.php">Top Scores grouped by class</a></font></p>
 <p><font face="Arial"><a href="bycompetitor.php">All runs sorted by competitor</a></font></p>
 
