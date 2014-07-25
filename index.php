7d9c7cb88e68c36ebf2d24fe788c7b10067afb44
scott2020 <scott@wolffm.com>
2014-07-25 11:58:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
 Shootout Results 2014
 <br>
+<?php
+include '../config.php';
+include('../register_globals.php');
+register_globals();
 <title>Reports - <?php echo $SITE_NAME;?></title>
 </html>
