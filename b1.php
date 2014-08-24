b861e91d7a2d614600584629e52d27104e6963e9
scott2020 <scott@wolffm.com>
2014-08-24 08:45:16 -0500

    Update b1.php

--- b1.php
+++ b1.php
@@ -1,9 +1,10 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Mobile Banner -->
+<!-- Shootout Responsive -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:50px"
+     style="display:block"
      data-ad-client="ca-pub-3815362766482748"
-     data-ad-slot="4637096717"></ins>
+     data-ad-slot="3889793117"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
