123d72caa812fa845c905b20683de165e991ce26
scott2020 <scott@wolffm.com>
2015-07-20 09:06:53 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // ** MySQL settings ** //
-$SITE_NAME = 'Shootout 2014dev';
-$DB_NAME = 'shootout13';
+$SITE_NAME = 'Shootout 2015';
+$DB_NAME = 'shootout15';
 $DB_USER = 'shootout13';
 $DB_PASS = 'fartman1';
 ?>
