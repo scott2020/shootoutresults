44d4bf02b5fc148e8142b2f18f950e7ace3c4f79
scott2020 <scott@wolffm.com>
2015-07-27 16:47:00 -0500

    Update b1.php

--- b1.php
+++ b1.php
@@ -1,9 +1,9 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Shootout Responsive -->
+<!-- ShootoutResults2015 -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-3815362766482748"
-     data-ad-slot="3889793117"
+     data-ad-slot="9275507118"
      data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
