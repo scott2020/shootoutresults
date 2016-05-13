085545e27c6b39f1e055181aaca7f71017e88b85
scott2020 <scott@wolffm.com>
2016-05-13 13:40:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 </center>
 <?php
 
-$con = mysql_connect("localhost", $DB_USER, $DB_PASS)or die("Connect Error: ".mysql_error());
+$con = mysql_connect($DB_HOST, $DB_USER, $DB_PASS)or die("Connect Error: ".mysql_error());
 mysql_select_db($DB_NAME,$con);
 # $con = mysql_connect("localhost", "shootout", "shootout")or die("Connect Error: ".mysql_error());
 # mysql_select_db("shootout",$con);
